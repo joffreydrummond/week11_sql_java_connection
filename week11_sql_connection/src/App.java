2af9fbafa7107bcea7048b8c30fae2b0a01977c9
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-03-31 11:37:51 -0700

    added query that pulls data from employees db

--- week11_sql_connection/src/App.java
+++ week11_sql_connection/src/App.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App {
 
@@ -9,8 +7,17 @@ public class App {
         String connectionString = "jdbc:mysql://localhost:3306/employees"; //localhost is an alias to 127.0.0.1 my
         // computer 3306 is the default port numbers
 
+        final String SELECT_QUERY = "SELECT * FROM employees LIMIT 200";
+
         try (Connection connection = DriverManager.getConnection(connectionString, "root", "password")) {
             System.out.println("We have connected successfully! You rock");
+            try (Statement statement = connection.createStatement()) {
+          ResultSet rs =  statement.executeQuery(SELECT_QUERY);
+          while(rs.next()){
+              System.out.println("emp no: " + rs.getInt(1) + " DOB: " + rs.getString(2) + " First Name: " + rs.getString(3)
+              + " Last Name: " + rs.getString(4));
+          }
+            }
         } catch (SQLException throwables) {
             System.out.println("Error connecting to the database.");
             throwables.printStackTrace();
