347c5b45558589ca0a9ea952d1bec785a4828b3c
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-01 11:12:37 -0700

    added select employee method

--- week11_sql_connection/src/App2.java
+++ week11_sql_connection/src/App2.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App2 {
     private static Connection conn;
@@ -13,6 +11,8 @@ public class App2 {
             System.out.println("Attempting DB connection....");
             conn = DriverManager.getConnection(connString,"root", "password");
             System.out.println("We have connected successfully! You rock");
+//            selectAllEmployees();
+            selectEmployee(10023);
         } catch (SQLException throwables) {
             System.out.println("Error connecting to the database.");
             throwables.printStackTrace();
@@ -24,4 +24,39 @@ public class App2 {
             }
         }
     }
+
+    public static void selectAllEmployees(){
+        String query = "SELECT * FROM employees LIMIT 250";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next()){
+                System.out.println("emp no: " + rs.getInt(1) + " DOB: " + rs.getString("birth_date") + " First Name: " + rs.getString(3)
+              + " Last Name: " + rs.getString(4));
+            }
+        } catch (SQLException throwables) {
+            System.out.println("Error when running SelectAllEmployees().");
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void selectEmployee(Integer employeeNum){
+        String query = "SELECT * FROM employees WHERE emp_no = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, employeeNum);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("emp no: " + rs.getInt(1) + " DOB: " + rs.getString("birth_date") + " First Name: " + rs.getString(3)
+                        + " Last Name: " + rs.getString(4));            }
+        } catch (SQLException throwables) {
+            System.out.println("Error when running SelectEmployee().");
+            throwables.printStackTrace();
+        }
+
+    }
+
+
 }
