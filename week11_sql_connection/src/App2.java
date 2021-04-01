4534cdcba74b31720b8b06b4f2e4da4bfece6ada
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-01 09:52:00 -0700

    added finally statement to catch and close db if connection is not successful

--- week11_sql_connection/src/App2.java
+++ week11_sql_connection/src/App2.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class App2 {
     private static Connection conn;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         final String connString = "jdbc:mysql://localhost:3306/employees";
         //localhost is an alias to 127.0.0.1 my computer 3306 is the default port numbers
@@ -16,6 +16,12 @@ public class App2 {
         } catch (SQLException throwables) {
             System.out.println("Error connecting to the database.");
             throwables.printStackTrace();
+        } finally {
+            if (conn != null){
+                System.out.println("Closing DB connection...");
+                conn.close();
+                System.out.println("DB has disconnected successfully.");
+            }
         }
     }
 }
