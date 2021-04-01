ca78e3eb4e245a156248c9e2e4ec0790b882c3f4
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-01 09:48:42 -0700

    created new app file. placed connection outside of psvm to increase scope

---
+++ week11_sql_connection/src/App2.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App2 {
+    private static Connection conn;
+    public static void main(String[] args) {
+
+        final String connString = "jdbc:mysql://localhost:3306/employees";
+        //localhost is an alias to 127.0.0.1 my computer 3306 is the default port numbers
+
+        try {
+            System.out.println("Attempting DB connection....");
+            conn = DriverManager.getConnection(connString,"root", "password");
+            System.out.println("We have connected successfully! You rock");
+        } catch (SQLException throwables) {
+            System.out.println("Error connecting to the database.");
+            throwables.printStackTrace();
+        }
+    }
+}
